7965a6f4cd4a73a7522dcf73e86037b936fc6728
Anton <smirnovanton123@icloud.com>
2023-02-18 00:37:44 +0300

    add main

--- src/test/java/org/example/ManegerTest.java
+++ src/test/java/org/example/ManegerTest.java
@@ -3,11 +3,11 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class mainTest {
+public class ManegerTest {
     @Test
 
     public void test() {
-        Main add = new Main();
+        Maneger add = new Maneger();
         add.addFilm("1film");
         add.addFilm("2film");
         add.addFilm("3film");
@@ -27,20 +27,27 @@ public class mainTest {
     }
     @Test
     public void test2() {
-        Main add = new Main();
+        Maneger add = new Maneger();
         add.addFilm("1film");
         add.addFilm("2film");
         add.addFilm("3film");
         add.addFilm("4film");
+        add.addFilm("5film");
+        add.addFilm("6film");
+        add.addFilm("7film");
+        add.addFilm("8film");
+        add.addFilm("9film");
+        add.addFilm("10film");
+        add.addFilm("11film");
 
 
-        String[] expectid = {"4film", "3film", "2film"};
-        String[] actual = add.findLast(3);
+        String[] expectid = { "11film","10film","9film","8film", "7film","6film", "5film", "4film", "3film","2film"};
+        String[] actual = add.findLast();
         Assertions.assertArrayEquals(expectid, actual);
     }
     @Test
     public void test3() {
-        Main add = new Main();
+        Maneger add = new Maneger();
         add.addFilm("1film");
         add.addFilm("2film");
         add.addFilm("3film");
@@ -48,24 +55,8 @@ public class mainTest {
 
 
         String[] expectid = {"4film", "3film", "2film","1film" };
-        String[] actual = add.findLast(4);
+        String[] actual = add.findLast();
         Assertions.assertArrayEquals(expectid, actual);
     }
-    @Test
-    public void test4() {
-        Main add = new Main();
-        add.addFilm("1film");
-        add.addFilm("2film");
-        add.addFilm("3film");
-        add.addFilm("4film");
-
-
-        String[] expectid = {"4film", "3film", "2film","1film" };
-        String[] actual = add.findLast(10);
-        Assertions.assertArrayEquals(expectid, actual);
-    }
-
 
 }
-
-
